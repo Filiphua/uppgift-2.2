a6b590dd2ac4ec2ee47eb62c29d3156de798eb13
Filip <fihu08001@utb.vaxjo.se>
2025-11-04 12:31:11 +0100

    Add project files.

---
+++ uppgift 2.2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+namespace uppgift_2._2
+{
+      class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur många meter hoppade Alma och Elin?");
+            int Almahopp = int.Parse(Console.ReadLine());
+            int Elinhopp= int.Parse(Console.ReadLine());
+            Console.WriteLine(Elinhopp - Almahopp);
+
+
+        }
+    }
+}

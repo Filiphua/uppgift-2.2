46fba645bec1d8a4cc53df33f82001ae5324f7ef
Filiphua <fihu08001@utb.vaxjo.se>
2025-11-06 08:19:32 +0100

    Update output messages for jump comparison

--- uppgift 2.2/Program.cs
+++ uppgift 2.2/Program.cs
@@ -7,11 +7,13 @@ namespace uppgift_2._2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hur många meter hoppade Alma och Elin?");
+            Console.WriteLine("Hur många meter hoppade Elin än Alma?");
             int Almahopp = int.Parse(Console.ReadLine());
             int Elinhopp= int.Parse(Console.ReadLine());
-            Console.WriteLine(Elinhopp - Almahopp);
-
+            Console.WriteLine("Alma hoppade " + Almahopp + " meter ");
+            Console.WriteLine("Elin hoppade " + Elinhopp + " meter. ");
+            int skillnad= (Elinhopp - Almahopp);
+            Console.WriteLine("Elin hoppade " + skillnad + " meter längre än Alma.");
 
         }
     }
